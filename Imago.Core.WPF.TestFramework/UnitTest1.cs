81c6f432de750bcf7adab9883facc06bb02eb010
Paulo Aboim Pinto <aboimpinto@gmail.com>
2020-09-12 02:52:38 +0200

    initial commit

---
+++ Imago.Core.WPF.TestFramework/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Imago.Core.WPF.TestFramework
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
